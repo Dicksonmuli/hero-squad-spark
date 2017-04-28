f102823eb2b59739529e73069832ae6c61c54f0d
dicksonmuli <dicksonmully6@gmail.com>
2017-04-28 16:28:18 +0300

    final changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -74,7 +74,7 @@ public class App {
 			return new ModelAndView(model, layout);
 		}, new VelocityTemplateEngine());
 		//search route
-		get("squads/search", (request, response) -> {
+		get("/squads/search", (request, response) -> {
 			Map<String, Object> model = new HashMap<String, Object>();
 			model.put("template", "templates/search.vtl");
 			return new ModelAndView(model, layout);
