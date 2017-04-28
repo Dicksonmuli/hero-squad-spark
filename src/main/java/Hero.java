ae15860af7d1e649cf4059ced55928632fc45201
dicksonmuli <dicksonmully6@gmail.com>
2017-04-28 08:04:21 +0300

    tests to check if methods return name, age, ability and strength

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,29 +1,45 @@
+// importing java libraries to be used
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+	// defining private variables
 	private String mName;
 	private int mAge;
 	private String mAbility;
 	private String mWeakness;
+	private static List<Hero> instances = new ArrayList<Hero>();
+	private int mId;
 
+//constructor that accepts name, age, ability and weakness as input
 	public Hero(String name, int age, String ability, String weakness) {
 		mName = name;
 		mAge = age;
 		mAbility = ability;
 		mWeakness = weakness;
-	}
+		instances.add(this);
+		mId = instances.size();
 
+	}
+//returns the name
 	public String getName() {
 		return mName;
 	}
-
+//returns the age
 	public int getAge() {
 		return mAge;
 	}
-
+//returns ability of the hero
 	public String getAbility() {
 		return mAbility;
 	}
-
+//returns weakness of the hero
 	public String getWeakness() {
 		return mWeakness;
+	}
+	
+//returns the id of the hero
+	public int getId() {
+		return mId;
 	}
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -3,9 +3,35 @@ import static org.junit.Assert.*;
 
 public class HeroTest {
 	private Hero testHero;
-
+	//executes before each test
 	@Before
 	public void instanciate() {
 		testHero = new Hero("NinjaMan",  30, "Strength, speed", "cannot run");
 	}
+//executes after every test
+	// @After
+	// public void tearDown() {
+	// 	Hero.clear();
+	// }
+//instantiates correctly
+	public void Hero_instantiatesCorrectly_true() {
+		assertTrue(testHero instanceof Hero);
+	}
+//getName returns hero's name
+	public void getName_returnsName_NinjaMan() {
+		assertEquals("NinjaMan", testHero.getName());
+	}
+//getAge returns hero's  age
+	public void getAge_returnsAge_30 () {
+		assertEquals(30, testHero.getAge());
+	}
+//getAbility returns hero's  ability
+	public void getAbility_returnsAbility_Strength() {
+		assertEquals("Strength, speed", testHero.getAbility());
+	}
+//getWeakness returns hero's weakness
+	public void getWeakness_returnsAbility_CannotRun() {
+		assertEquals("cannot run", testHero.getWeakness());
+	}
+
 }
