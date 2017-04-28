26b5e8a847658683e01148adf63d4c48dee91413
dicksonmuli <dicksonmully6@gmail.com>
2017-04-28 12:53:17 +0300

    add squad form addition

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,7 +15,23 @@ public class App {
 			model.put("template", "templates/index.vtl");
 			return new ModelAndView(model, layout);
 		}, new VelocityTemplateEngine());
-
-
+		//displays all squads
+		get("/squads", (request, response) -> {
+			Map<String, Object> model = new HashMap<String, Object>();
+			model.put("squads", Squad.all());
+			model.put("template", "templates/squads.vtl");
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine());
+		//retrieves a form to add new squad form
+		get("/squads/new", (request, response) -> {
+			Map<String, Object> model = new HashMap<String, Object>();
+			model.put ("template", "templates/squads-form.vtl");
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine());
+		//adds new squads with verb post
+		// get("/squads", (request, response) -> {
+		// 	Map<String, Object> model = new HashMap<String, Object>();
+		//
+		// })
 	}
 }
