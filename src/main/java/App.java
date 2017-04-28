a484627ba88842f056cbaab47d10c438840c6745
dicksonmuli <dicksonmully6@gmail.com>
2017-04-28 14:10:47 +0300

    new heroes route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -29,9 +29,32 @@ public class App {
 			return new ModelAndView(model, layout);
 		}, new VelocityTemplateEngine());
 		//adds new squads with verb post
-		// get("/squads", (request, response) -> {
-		// 	Map<String, Object> model = new HashMap<String, Object>();
-		//
-		// })
+		post("/squads", (request, response) -> {
+			Map<String, Object> model = new HashMap<String, Object>();
+			String team = request.queryParams("team");
+			String reason = request.queryParams("reason");
+			Integer max = Integer.parseInt(request.queryParams("max"));
+			Squad newSquad = new Squad(team, max, reason);
+			model.put("template", "template/squad-success.vtl");
+			// System.out.println(newSquad);
+			// response.redirect("/squads/"+newSquad.getId());
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine());
+		//details of a specific squad
+		get("/squads/:id", (request, response) -> {
+			Map<String, Object> model = new HashMap<String, Object>();
+			Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+			model.put("squad", squad);
+			model.put("template", "templates/squad.vtl");
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine());
+
+		get("squads/:id/heroes/new", (request, response) -> {
+			Map<String, Object> model = new HashMap<String, Object>();
+			Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+			model.put("squad", squad);
+			model.put("template", "template/hero-form.vtl");
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine());
 	}
 }
